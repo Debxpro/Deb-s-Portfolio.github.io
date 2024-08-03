0044b109e32686ff0e75b69aaff57462bfc3dc0d
Debxpro <debayan.lahiry@gmail.com>
2024-08-03 18:54:54 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+    strings : ["Cybersecurity Student." , "Web Developer." , "Ethical Hacker."],
+    typespeed: 100,
+    backspeed: 100,
+    backdelay: 1000,
+    loop: true
+})
+
